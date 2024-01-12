a4ff6767bc5446501b822986b9ae17909dd28857
Mehdi Karami <90229452+nsswifter@users.noreply.github.com>
2024-01-12 15:10:02 +0330

    Add Documentation

--- RockPaperScissors/RockPaperScissors.jl
+++ RockPaperScissors/RockPaperScissors.jl
@@ -1,28 +1,37 @@
 # Rock 🪨, Paper 📃, Scissors ✂️ Game
 
-function determine_winner(computer_move, human_move)
+# Determine and display the winner
+function determine_winner(computer_move::String, human_move::String)
     if computer_move == human_move
+        # If both moves are the same, it's a tie
         println("It's a tie! Please try again.")
     elseif (computer_move == "rock" && human_move == "scissors") ||
            (computer_move == "paper" && human_move == "rock") ||
            (computer_move == "scissors" && human_move == "paper")
+        # If the computer wins based on move combinations, notify the user
         print("You lose, the computer won with $computer_move, please try again")
     else
+        # If none of the above conditions are met, the human wins
         print("You won, the computer lost with $computer_move, nice work!")
     end
 end
 
+# Function to play the Rock, Paper, Scissors game
 function play_rock_paper_scissors()
+    # Possible moves in the game
     moves = ["rock", "paper", "scissors"]
+
+    # Randomly select a move for the computer
     computer_move = rand(moves)
 
+    # Prompt the user to enter their move
     human_move = Base.prompt("Please enter rock, paper, or scissors")
 
+    # Displaying countdown before revealing moves
     for move in moves 
         println("$move...")
         sleep(0.8)
     end
-    
     println("Shoot!", '\n')
 
     determine_winner(computer_move, human_move)
