eba514e3e663454e6dc9a45d6b7fab9b84719ac9
Mehdi Karami <karami.mehdi.work@gmail.com>
2024-01-18 14:24:52 +0330

    Refactor Process Function For Clarification

--- UsafeOfUnion/UnionSample.jl
+++ UsafeOfUnion/UnionSample.jl
@@ -1,16 +1,16 @@
 # Define a union type for valid input types
-input_type = Union{Int, Float64, String}
+InputType = Union{Int, Float64, String}
 
 # Function that performs different operations based on input type
-function process_input(value::input_type)
+function process_input(value::InputType)::String
     if isa(value, Int)
-        return "Received an integer: $value"
+        "Received an integer: $value"
     elseif isa(value, Float64)
-        return "Received a float: $value"
+        "Received a float: $value"
     elseif isa(value, String)
-        return "Received a string: $value"
+        "Received a string: $value"
     else
-        return "Unsupported input type"
+        "Unsupported input type"
     end
 end
 
