cf51f717050b5cfb877137699f5b7207d1a0e6ff
Mehdi Karami <karami.mehdi.work@gmail.com>
2024-01-18 14:21:33 +0330

    Declare Checker Function

--- UsafeOfUnion/UnionSample.jl
+++ UsafeOfUnion/UnionSample.jl
@@ -1,2 +1,15 @@
 # Define a union type for valid input types
 input_type = Union{Int, Float64, String}
+
+# Function that performs different operations based on input type
+function process_input(value::input_type)
+    if isa(value, Int)
+        return "Received an integer: $value"
+    elseif isa(value, Float64)
+        return "Received a float: $value"
+    elseif isa(value, String)
+        return "Received a string: $value"
+    else
+        return "Unsupported input type"
+    end
+end
