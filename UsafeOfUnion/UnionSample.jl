0af98bbf1118deb6fba562b6f56718b8530dc050
Mehdi Karami <karami.mehdi.work@gmail.com>
2024-01-18 14:43:43 +0330

    Implement Process Function in 2 Approaches

--- UsafeOfUnion/UnionSample.jl
+++ UsafeOfUnion/UnionSample.jl
@@ -1,7 +1,9 @@
 # Define a union type for valid input types
 InputType = Union{Int, Float64, String}
 
-# Function that performs different operations based on input type
+# Functions that perform different operations based on input type
+
+# First Approach of Control Flow
 function process_input(value::InputType)::String
     if isa(value, Int)
         "Received an integer: $value"
@@ -12,6 +14,17 @@ function process_input(value::InputType)::String
     else
         "Unsupported input type"
     end
+end
+
+# Second Approach of Control Flow
+# This version of the function will be called.
+# The first version is immediately redefined by the second version,
+# and the second definition takes precedence.
+function process_input(value::InputType)::String
+    isa(value, Int) ? "Received an integer: $value" :
+    isa(value, Float64) ? "Received a float: $value" :
+    isa(value, String) ? "Received a string: $value" :
+    "Unsupported input type"
 end
 
 # Examples
